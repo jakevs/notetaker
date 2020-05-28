6650b982d37f36d732865cd28d697fc1b751afa6
James Van Slyke <jakevs3@gmail.com>
2020-05-27 19:11:42 -0500

    got html routes working

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const htmlroutes = require("./routes/htmlroutes");
+const apiroutes = require("./routes/apiroutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 //routes
-app.use("/");
+app.use("/", htmlroutes);
+
+//listener
+app.listen(PORT, function () {
+  console.log("App is listening on PORT: " + PORT);
+});
